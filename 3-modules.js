4591912ef9e43a95739159aa94e439189c063725
dimondsword <claudewonkem@gmail.com>
2023-01-16 19:55:02 +0100

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,9 @@
+// Modules
+
+const names = require("./4-names");
+const sayHi = require("./5-utilis");
+const data = require("./6-alt");
+require("./7-mind-grenade");
+// sayHi("susan");
+// sayHi(names.john);
+// sayHi(names.peter);
